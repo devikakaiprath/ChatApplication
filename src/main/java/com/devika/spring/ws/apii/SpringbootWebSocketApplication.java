8dbef13454ea4ddc55a2651ec806b33c68426b52
Devika K <devikak2000kvr@gmail.com>
2025-01-08 11:35:43 +0530

    Add files via upload

---
+++ src/main/java/com/devika/spring/ws/apii/SpringbootWebSocketApplication.java
@@ -0,0 +1,15 @@
+package com.devika.spring.ws.apii;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebSocketApplication.class, args);
+	}
+
+}
